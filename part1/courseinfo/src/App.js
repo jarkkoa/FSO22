180138994c6725bb3bd6e4840eda479b43dc1967
Jarkko Ahvenniemi <Jarge98@gmail.com>
2022-06-29 11:43:50 +0300

    Courseinfo step 5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -37,31 +37,32 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
     
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-  
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-  
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+    
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   
 
   return (
     <div>
-      <Header name={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
