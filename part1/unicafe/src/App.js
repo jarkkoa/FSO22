bb1757a0a63677409d326b290f7f4c7b4ea44edc
Jarkko Ahvenniemi <Jarge98@gmail.com>
2022-07-01 11:52:55 +0300

    Anecdotes step 1 (1.12)

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -57,6 +57,18 @@ const App = () => {
   const [pos, setPos] = useState(0)
   const [all, setAll] = useState(0)
 
+  const [anecIndex, setAnecdote] = useState(0)
+
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ]
+
   const handleGoodClick = () => {
 
     const newGood = good + 1;
@@ -96,6 +108,10 @@ const App = () => {
       <Button text='bad' action={handleBadClick}></Button>
 
       <Statistics good={good} neutral={neutral} bad={bad} avg={avg} pos={pos} all={all} />
+
+      <h1>Anecdotes</h1>
+      <p>{anecdotes[anecIndex]}</p>
+      <Button text='Next anecdote' action={() => setAnecdote(Math.floor(Math.random() * anecdotes.length))}></Button>
 
     </div>
   )
